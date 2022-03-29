d985975f54c37e3409640659f34eed9fb80d0566
ChoiSeungho <tizkzks96@naver.com>
2022-03-29 15:46:37 +0900

    add fist

--- Assets/Scripts/FishGenerator.cs
+++ Assets/Scripts/FishGenerator.cs
@@ -30,7 +30,14 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
 using UnityEngine;
+using UnityEngine.Jobs;
+
+using math = Unity.Mathematics.math;
+using random = Unity.Mathematics.Random;
 
 public class FishGenerator : MonoBehaviour
 {
@@ -48,19 +55,60 @@ public class FishGenerator : MonoBehaviour
     public float swimSpeed;
     public float turnSpeed;
 
+    private NativeArray<Vector3> velocities;
+    private TransformAccessArray transformAccessArray;
+
+    private PositionUpdateJob positionUpdateJob;
+
+    private JobHandle positionUpdateJobHandle;
+
     private void Start()
     {
-        
+        velocities = new NativeArray<Vector3>(amountOfFish, Allocator.Persistent);
+
+        transformAccessArray = new TransformAccessArray(amountOfFish);
+
+        for (int i = 0; i < amountOfFish; i++)
+        {
+
+            float distanceX =
+            Random.Range(-spawnBounds.x / 2, spawnBounds.x / 2);
+
+            float distanceZ =
+            Random.Range(-spawnBounds.z / 2, spawnBounds.z / 2);
+
+            Vector3 spawnPoint =
+            (transform.position + Vector3.up * spawnHeight) + new Vector3(distanceX, 0, distanceZ);
+
+            Transform t =
+            (Transform)Instantiate(objectPrefab, spawnPoint,
+            Quaternion.identity);
+
+            transformAccessArray.Add(t);
+        }
     }
 
     private void Update()
     {
-      
+        positionUpdateJob = new PositionUpdateJob()
+        {
+            objectVelocities = velocities,
+            jobDeltaTime = Time.deltaTime,
+            swimSpeed = this.swimSpeed,
+            turnSpeed = this.turnSpeed,
+            time = Time.time,
+            swimChangeFrequency = this.swimChangeFrequency,
+            center = waterObject.position,
+            bounds = spawnBounds,
+            seed = System.DateTimeOffset.Now.Millisecond
+        };
+
+        positionUpdateJobHandle = positionUpdateJob.Schedule(transformAccessArray);
     }
 
     private void LateUpdate()
     {
-        
+        positionUpdateJobHandle.Complete();
     }
 
     private void OnDrawGizmos()
@@ -71,6 +119,80 @@ public class FishGenerator : MonoBehaviour
 
     private void OnDestroy()
     {
+        transformAccessArray.Dispose();
+        velocities.Dispose();
+    }
 
+    [BurstCompile]
+    struct PositionUpdateJob : IJobParallelForTransform
+    {
+        public NativeArray<Vector3> objectVelocities;
+
+        public Vector3 bounds;
+        public Vector3 center;
+
+        public float jobDeltaTime;
+        public float time;
+        public float swimSpeed;
+        public float turnSpeed;
+        public int swimChangeFrequency;
+
+        public float seed;
+
+
+
+        public void Execute(int i, TransformAccess transform)
+        {
+            Vector3 currentVelocity = objectVelocities[i];
+
+            random randomGen = new random((uint)(i * time + 1 + seed));
+
+            transform.position +=
+            transform.localToWorldMatrix.MultiplyVector(new Vector3(0, 0, 1)) *
+            swimSpeed *
+            jobDeltaTime *
+            randomGen.NextFloat(0.3f, 1.0f);
+
+            if (currentVelocity != Vector3.zero)
+            {
+                transform.rotation =
+                Quaternion.Lerp(transform.rotation,
+                Quaternion.LookRotation(currentVelocity), turnSpeed * jobDeltaTime);
+            }
+
+            Vector3 currentPosition = transform.position;
+
+            bool randomise = true;
+
+            if (currentPosition.x > center.x + bounds.x / 2 ||
+                currentPosition.x < center.x - bounds.x / 2 ||
+                currentPosition.z > center.z + bounds.z / 2 ||
+                currentPosition.z < center.z - bounds.z / 2)
+            {
+                Vector3 internalPosition = new Vector3(center.x +
+                randomGen.NextFloat(-bounds.x / 2, bounds.x / 2) / 1.3f,
+                0,
+                center.z + randomGen.NextFloat(-bounds.z / 2, bounds.z / 2) / 1.3f);
+
+                currentVelocity = (internalPosition - currentPosition).normalized;
+
+                objectVelocities[i] = currentVelocity;
+
+                transform.rotation = Quaternion.Lerp(transform.rotation,
+                Quaternion.LookRotation(currentVelocity),
+                turnSpeed * jobDeltaTime * 2);
+
+                randomise = false;
+            }
+
+            if (randomise)
+            {
+                if (randomGen.NextInt(0, swimChangeFrequency) <= 2)
+                {
+                    objectVelocities[i] = new Vector3(randomGen.NextFloat(-1f, 1f),
+                    0, randomGen.NextFloat(-1f, 1f));
+                }
+            }
+        }
     }
 }
